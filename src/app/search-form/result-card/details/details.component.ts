bc0b1041e71b702769f39d73a8f0815deb169850
raghuvulli2000 <vulli@usc.edu>
2022-10-29 05:09:38 -0700

    Table

--- src/app/search-form/result-card/details/details.component.ts
+++ src/app/search-form/result-card/details/details.component.ts
@@ -9,6 +9,7 @@ import { BackendApiService } from 'src/app/services/backend-api.service';
 export class DetailsComponent implements OnInit {
  @Input() detailData: any;
  @Input() reviewsData: any;
+ filteredData: any[][] = [];
 mapOptions: google.maps.MapOptions;
 marker;
   constructor(private backendapi: BackendApiService) { }
@@ -22,8 +23,31 @@ marker;
 this.marker = {
    position: { lat: this.detailData.coordinates.latitude, lng: this.detailData.coordinates.longitude },
 }
-  }
 
+     var combine = "";
+  var categories = this.detailData.categories;
+    for (var i = 0; i < categories.length - 1; i++) {
+    combine += categories[i].title + " | ";
+  }
+  var status = "Closed";
+  if(this.detailData.hours[0].is_open_now){
+    status = "Open Now"
+  }
+ 
+
+  combine += categories[categories.length - 1].title;
+ this.filteredData.push(["Address", this.detailData.location.display_address.join(" ")]);
+this.filteredData.push(["Category", combine]);
+this.filteredData.push(["Phone", this.detailData.display_phone]);
+this.filteredData.push(["Price range", this.detailData.price]);
+this.filteredData.push(["Status", status]);
+//this.filteredData.push(["Visit Yelp For More", this.detailData.url]);
+   console.log(this.filteredData);
+
+  }
+filterData(){
+
+}
   goBack(){
     this.backendapi.backEvent.emit();
   }
